d09d6d7e876e12828972680df80e1ab271f75843
Pal-cloud <paloma.gom.sal@gmail.com>
2025-08-29 13:16:38 +0200

    feat(models): start ButterflyModel with Sequelize and partial validations

--- models/ButterflyModel.js
+++ models/ButterflyModel.js
@@ -1,1 +1,24 @@
-...
+import { DataTypes } from "sequelize";
+import db_connection from "../database/db_connection.js";
+
+const ButterflyModel = db_connection.define('butterflies', {
+
+    common_name: { 
+
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'especificar common_name'
+            },
+            len: {
+                min: 2,
+                msg: 'el campo common_name no permite menos de 2 caracteres'
+            }
+        }
+    },
+    scientific_name
+
+    });
+
+
