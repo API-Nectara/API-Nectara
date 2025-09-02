269173f254a65ad830a4f0a44952c5166a8322d0
gemayc <gemita284@hotmail.com>
2025-09-02 14:34:52 +0200

    feat(controllers): add getOneButterfly and deleteButterfly functions, feat(routes): add GET and DELETE routes for butterflies by ID

--- controllers/ButterflyController.js
+++ controllers/ButterflyController.js
@@ -1,10 +1,36 @@
 import ButterflyModel from "../models/ButterflyModel.js";
 
-export const getAllButterflies = async(req, res) => {
+export const getAllButterflies = async (req, res) => {
     try {
         const butterflies = await ButterflyModel.findAll()
         res.status(200).json(butterflies)
     } catch (error) {
-        res.json({message: error.message})
+        res.json({ message: error.message })
     }
 }
+
+export const getOneButterfly = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const butterfly = await ButterflyModel.findByPk(id);
+
+        if (!butterfly) {
+            return res.status(404).json({ error: "Butterfly no found" });
+        }
+        res.json(butterfly)
+
+    } catch (error) {
+        console.error("getOneButterfly error:", error);
+        res.status(500).json({ error: "Error obteniendo butterfly" });
+
+    }
+};
+export const deleteButterfly = async (req, res) => {
+    try {
+        const butterfly = await ButterflyModel.destroy({ where: { id: req.params.id } });
+        res.status(200).json({ message: "The butterfly has been deleted successfully!" });
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+
+};
--- routes/butterflyRoutes.js
+++ routes/butterflyRoutes.js
@@ -1,1 +1,10 @@
-...
+import express from "express"
+import { getAllButterflies, deleteButterfly, getOneButterfly } from "../controllers/ButterflyController.js"
+
+const butterflyRouter= express.Router()
+
+butterflyRouter.get("/", getAllButterflies)
+butterflyRouter.get("/:id", getOneButterfly)
+butterflyRouter.delete("/:id", deleteButterfly)
+
+export default butterflyRouter
